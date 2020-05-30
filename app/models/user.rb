f63a44f01486562bcff626cb9b1e86b68ca0dc9b
Apple <apple@Apple-no-MacBook-Pro.local>
2020-05-30 17:59:50 +0900

    README

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ApplicationRecord
   has_many :circle_joins, class_name: "Join", dependent: :destroy
   has_many :joined_circles, source: "circle", through: :circle_joins
   has_many :host_circles, dependent: :destroy
-  has_many :user_chats, source: :circles, through: :host_circles
   attachment :profile_image
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
